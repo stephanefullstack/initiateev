a17de56f80cec385d31094d39d404462e1a7d459
Stéphane ZE-OGIER <contact@ze-ogier.com>
2020-08-30 23:15:22 +0200

    front index and new project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,19 +1,38 @@
 class ProjectsController < ApplicationController
   def index
+    @projects = Project.all
   end
 
   def show
+    @project = Project.find(params[:id])
   end
 
   def new
+    @project = Project.new
   end
 
   def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to projects_path
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    @project.delete
+    redirect_to projects_path
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title, :photo)
   end
 end
